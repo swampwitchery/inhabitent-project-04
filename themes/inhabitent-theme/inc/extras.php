f12963cb57d6e190b80f6bf43417d1cdfbfbf7f9
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-02-15 14:09:01 -0800

    second try with updates

--- themes/inhabitent-theme/inc/extras.php
+++ themes/inhabitent-theme/inc/extras.php
@@ -23,7 +23,25 @@ add_filter( 'body_class', 'red_starter_body_classes' );
 
 
 function inhabitent_login_logo() {
-	echo '<h1> Does this work? </h1>';
-}
+	$logo_url = get_template_directory_uri().'/images/logos/inhabitent-logo-text-dark.svg';
 
+	echo '<style> 
+	.login h1 a {
+		background-image: url(' . $logo_url . ');
+		background-repeat: no-repeat;
+		background-size: 300px 53px;
+		padding-left: 230px;
+	}
+
+	.login .button.button-primary {
+		background-color: #248A83;
+	}
+		</style>';
+}
 add_action( 'login_head', 'inhabitent_login_logo' );
+
+function inhabitent_login_url() {
+return get_site_url();
+}
+add_filter('login_headerurl', 'inhabitent_login_url');
+
