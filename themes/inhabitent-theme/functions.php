57d1d5be477a1c8d3ec7a24fbfb18e3eda5d9b03
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-02-17 18:35:47 -0800

    update on README.md and functions.php

--- themes/inhabitent-theme/functions.php
+++ themes/inhabitent-theme/functions.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package inhabitent_theme
+ * @package RED_Starter_Theme
  */
 
 if ( ! function_exists( 'inhabitent_theme_setup' ) ) :
