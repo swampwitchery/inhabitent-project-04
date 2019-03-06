f6bdc8a21808dc9674bd7573c0cf6cca85824ac2
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-03-05 23:17:12 -0800

    Style for Journal page. Working on image sizes, side bar, etc

--- themes/inhabitent-theme/home.php
+++ themes/inhabitent-theme/home.php
@@ -9,18 +9,10 @@ get_header(); ?>
 
 	<div id="primary" class="journal-content-area">
 		<main id="main" class="site-main journal-page" role="main">
-			<!-- <div class="single-post-loop">
-
-		<?php if ( have_posts() ) : ?>
 
 			 <?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?> 
-			</div> -->
+			
 			<div class="article-area">
-				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'template-parts/content' ); ?>
--- themes/inhabitent-theme/template-parts/content.php
+++ themes/inhabitent-theme/template-parts/content.php
@@ -14,9 +14,11 @@
 		<?php endif; ?>
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
+		
 		<div class="entry-meta">
+			
+		<?php if ( 'post' === get_post_type() ) : ?>
+		
 			<?php inhabitent_theme_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_theme_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
